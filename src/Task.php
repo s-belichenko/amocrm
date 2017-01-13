28c21ca7c764520064b48f7e4dd328c03f3fb47e
Maksim Klimenko <mavsan@gmail.com>
2017-01-13 15:46:49 +1000

    Комментирование класса управления Задачами.

--- src/Task.php
+++ src/Task.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Управление Задачами
+ */
 namespace AmoCRM;
 
 class Task extends Entity
@@ -17,48 +19,92 @@ class Task extends Entity
 
     const TYPE_CONTACT = 1; // Првязка к контакту
     const TYPE_LEAD = 2; // Привязка к сделке
-
+    
+    /**
+     * Task constructor.
+     */
     public function __construct()
     {
         $this->key_name = 'tasks';
         $this->url_name = $this->key_name;
     }
-
+    
+    /**
+     * Идентификатор контакта или сделки
+     * @param $value
+     *
+     * @return $this
+     */
     public function setElementId($value)
     {
         $this->element_id = $value;
 
         return $this;
     }
-
+    
+    /**
+     * Тип привязываемого элемента (1 - контакт, 2- сделка, 3 - компания, константы)
+     * @param $value
+     *
+     * @return $this
+     */
     public function setElementType($value)
     {
         $this->element_type = $value;
 
         return $this;
     }
-
+    
+    /**
+     * Тип задачи (см константы)
+     * @param $value
+     *
+     * @return $this
+     */
     public function setTaskType($value)
     {
         $this->task_type = $value;
 
         return $this;
     }
-
+    
+    /**
+     * Ответственный пользователь, ид
+     *
+     * @param $value
+     *
+     * @return $this
+     */
     public function setResponsibleUserId($value)
     {
         $this->responsible_user_id = $value;
 
         return $this;
     }
-
+    
+    /**
+     * Дата до которой необходимо завершить задачу. Если указано время 23:59,
+     * то в интерфейсах системы вместо времени будет отображаться "Весь день".
+     * Тип - time()
+     *
+     * @param $value
+     *
+     * @return $this
+     */
     public function setCompleteTill($value)
     {
         $this->complete_till = $value;
 
         return $this;
     }
-
+    
+    /**
+     * Текст задачи
+     *
+     * @param $value
+     *
+     * @return $this
+     */
     public function setText($value)
     {
         $this->text = $value;
