cde29afdefabb029cd3d9c32d2c606d2d5ef0f72
Maksim Klimenko <mavsan@gmail.com>
2017-01-13 11:56:53 +1000

    Добавлен именованный класс исключений.

---
+++ src/AmoCRMException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * AmoCRMException.php
+ * Date: 13.01.2017
+ * Time: 10:20
+ * Author: Maksim Klimenko
+ * Email: mavsan@gmail.com
+ */
+
+namespace AmoCRM;
+
+
+class AmoCRMException extends \Exception
+{
+    
+}
